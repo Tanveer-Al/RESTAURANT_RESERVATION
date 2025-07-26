bc16c0067fb094b086d38066ed9c424193041815
Tanveer Alam <tanveeralam2123@gmail.com>
2025-07-26 20:20:41 +0530

    yuhi

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     "/api": "https://restaurant-reservation-8t43.onrender.com/",
-  //   },
-  // },
   plugins: [react()],
 });
